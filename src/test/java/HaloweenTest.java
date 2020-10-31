932350b3d9800265cb51cfdffdfeae82340bc1d2
DilafruzR <dilafruz81@gmail.com>
2020-10-31 12:52:11 -0400

    initial

--- src/test/java/HaloweenTest.java
+++ src/test/java/HaloweenTest.java
@@ -1,6 +1,7 @@
 public class HaloweenTest {
 //I am about give everyone candy for Halloween
 //stay home and be safe and wear your mask outside
+    //hello world
 public static void main(String[] args) {
     System.out.println("It is in GitHub");
 }
